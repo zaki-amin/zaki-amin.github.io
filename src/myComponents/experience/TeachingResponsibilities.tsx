cfc5b8b954d5a07dd8812ed0373a024064906289
Zaki Amin <zakiamin0402@gmail.com>
2025-10-05 19:53:59 +0100

    feat: add afhea qualification

--- src/myComponents/experience/TeachingResponsibilities.tsx
+++ src/myComponents/experience/TeachingResponsibilities.tsx
@@ -1,4 +1,6 @@
 import { FC } from 'react'
+import HoverLink from '@/myComponents/navigation/HoverLink'
+import { PaintbrushIcon, School2Icon } from 'lucide-react'
 
 const TeachingResponsibilities: FC = () => {
   return (
@@ -10,8 +12,13 @@ const TeachingResponsibilities: FC = () => {
         </li>
         <li>
           I supervise human-centred design (HCD) projects at the intersection of
-          software engineering and design, in collaboration with the Royal
-          College of Art.
+          software engineering and design, in collaboration with the{' '}
+          <HoverLink
+            href='https://www.rca.ac.uk/'
+            text='Royal College of Art'
+            icon={<PaintbrushIcon size='12' />}
+          />
+          .
         </li>
         <li>
           I help deliver the Software Systems Engineering module, teaching the
@@ -21,6 +28,15 @@ const TeachingResponsibilities: FC = () => {
         </li>
         <li>
           I supervise research projects in the area of learning analytics.
+        </li>
+        <li>
+          I am an Associate Fellow of the{' '}
+          <HoverLink
+            href='https://www.advance-he.ac.uk/'
+            text='Higher Education Academy'
+            icon={<School2Icon size='12' />}
+          />
+          .
         </li>
       </ul>
     </div>
